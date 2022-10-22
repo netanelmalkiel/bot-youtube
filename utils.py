e75c87e32e04a579c4b6bbffee98436fa2474e46
netanelmalkiel <nati.malkiel@gmail.com>
2022-10-22 22:36:14 +0300

    change utils.py

--- utils.py
+++ utils.py
@@ -30,8 +30,7 @@ def calc_backlog_per_instance(sqs_queue_client, asg_client, asg_group_name):
             backlog_per_instance = msgs_in_queue / asg_size
 
         logger.info(f'backlog per instance: {backlog_per_instance}')
-        AWS_REGION = 'us-east-1'
-        cloudwatch = boto3.client('cloudwatch', region_name=AWS_REGION)
+        cloudwatch = boto3.client('cloudwatch', region_name='us-east-1')
         cloudwatch.put_metric_data(
             Namespace='tg-youtube-bot',
             MetricData=[
